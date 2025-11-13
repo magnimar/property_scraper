d681d50ffb272164e1663b193396a1053b2a23b8
Magni Mar Magnason <magni@optitog.com>
2025-11-13 20:49:08 +0000

    disable gpu

--- scraper.py
+++ scraper.py
@@ -123,6 +123,8 @@ def scrape_visir_properties():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.binary_location = "/usr/bin/chromium"
     driver = webdriver.Chrome(service=service, options=options)
 
     print(f"Opening browser and navigating to {start_url}...")
